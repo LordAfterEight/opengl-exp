26ee979bd1b50f195c8e89ce598a081e8fcf7856
LordAfterEight <elias@stettmayer.de>
2025-07-16 15:36:26 +0200

    Implemented object rendering

---
+++ helpers/shaders.h
@@ -0,0 +1,72 @@
+#include <iostream>
+#include "../include/glad/glad.h"
+#include <GLFW/glfw3.h>
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+
+const char *vertexShaderSource = 
+    "#version 460 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main() {\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource =
+    "#version 460 core \n"
+    "out vec4 FragColor;\n"
+    "void main() { \n"
+    "   FragColor = vec4(0.2f, 0.5f, 0.5f, 1.0f);"
+    "}\0";
+
+int setup_shaders() {
+    // --- Set up vertex shader ---
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    std::cout << "Compiling vertex shader..." << std::endl;
+    glCompileShader(vertexShader);
+
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if(!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    } else {
+        std::cout << "Vertex shader compiled successfully" << std::endl;
+    }
+
+    // --- Set up fragment shader ---
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    std::cout << "Compiling fragment shader..." << std::endl;
+    glCompileShader(fragmentShader);
+    
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if(!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    } else {
+        std::cout << "Fragment shader compiled successfully" << std::endl;
+    }
+
+    // --- Set upo shader program ---
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 #include "helpers/helpers.h"
 #include "helpers/shaders.h"
+#include "helpers/objects.h"
 using namespace std;
 
 const float WINDOW_SCALING = 0.5;
@@ -12,15 +13,21 @@ const int WINDOW_HEIGHT = 1080 * WINDOW_SCALING;
 int main() {
     GLFWwindow* win = init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "UwU", nullptr, nullptr);
 
-	glClearColor(.1F, .1F, .1F, 1.0F);
+	glClearColor(.0F, .0F, .0F, 1.0F);
 
-    setup_shaders();
+    unsigned int shaderProgram = setup_shaders();
+    unsigned int VAO = build_vertices();
+
 
 	while (!glfwWindowShouldClose(win)) {
 		glClear(GL_COLOR_BUFFER_BIT);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
         process_input(win);
 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(win);
 		glfwPollEvents();
 	}
